bdc78e724cc6e07877f0570ac3881b95298126b6
wastech <fataiwasiu2@gmail.com>
2022-04-26 00:50:54 +0100

    frontend setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-layout>
+    <router-view />
+  </q-layout>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+  <h2>awesome</h2>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
